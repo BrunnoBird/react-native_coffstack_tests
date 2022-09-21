eb0ca9585427b5c0c6d87b24c7f29c576125fa61
brunnobird <bird.tec.gonzalez@gmail.com>
2022-09-20 22:43:06 -0300

    finalizacao aula 02

---
+++ src/components/StarRating/__tests__/StarRating.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import {StarRating} from '../StarRating';
+
+describe('StarRting', () => {
+  describe('rating was passed', () => {
+    it('show the average', () => {
+      const {getByText} = render(<StarRating rating={{average: 7}} />);
+
+      expect(getByText('7')).toBeTruthy();
+    });
+    it('show the star icon', () => {
+      const {getByTestId} = render(<StarRating rating={{average: 7}} />);
+
+      expect(getByTestId('starIcon')).toBeTruthy();
+    });
+  });
+  describe('rating was not passing', () => {
+    it('return nothing', () => {
+      const {container} = render(<StarRating />);
+
+      expect(container.children.length).toEqual(0);
+    });
+  });
+});
